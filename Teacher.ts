808beb50458e10fbaa2ac11c1270c00c04dcad7f
Muhammad Naeem <161160123+Muhammad-Naeem22@users.noreply.github.com>
2024-03-24 15:36:11 +0500

    Frist Module Program

---
+++ Teacher.ts
@@ -0,0 +1,7 @@
+export default class Login
+
+{
+
+Data= "Login Done"
+
+}
---
+++ modules.js
@@ -0,0 +1,9 @@
+import slogin from "./Student.js";
+import tlogin from "./Teacher.js";
+import flogin from "./faculty.js";
+let teacher = new tlogin();
+console.log(teacher.Data);
+let student = new slogin();
+console.log(student.Data);
+let faculty = new flogin();
+console.log(faculty.Data);
